b18fbc4dbcd5fa50fb0879cc81b91d1af8c60541
amandeep <samandeepkaue34@gmail.com>
2024-07-26 17:22:35 -0400

    datasource created to store data to database

--- src/main/java/com/example/securityDemo/SecurityConfig.java
+++ src/main/java/com/example/securityDemo/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.securityDemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,7 +13,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,6 +24,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -36,16 +43,20 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(){
-
-        UserDetails user1 = User.withUsername("admin")
+        // in memory user authentication
+        UserDetails admin = User.withUsername("admin")
                 .password("{noop}password1")
                 .roles("ADMIN")
                 .build();
-        UserDetails user2 = User.withUsername("user1")
+        UserDetails user1 = User.withUsername("user1")
                 .password("{noop}password2")
                 .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(user1 ,user2);
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(admin);
+        userDetailsManager.createUser(user1);
+        return userDetailsManager;
+        //return new InMemoryUserDetailsManager(admin ,user1);
     }
 
 }
